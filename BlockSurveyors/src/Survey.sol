f37a163392b50ddfba2d18c53fb0c7461ba6433a
madison <longmadison@gmail.com>
2024-05-30 17:36:33 -0700

    added many new functions, see comments for details

--- BlockSurveyors/src/Survey.sol
+++ BlockSurveyors/src/Survey.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.24;
 contract Survey {
     //FUNCTIONS TO INCLUDE:
     // endNow()
-    // vote()
+    
     // getResults()
     
+    
     address private owner;
-    uint256 id;
-    registered_user user; // name of registered_user
-    string problem; // the question the user asks
+    uint256 public id;
+    mapping (address => bool) public hasVoted; //mapping of addresses to whether or not they have voted (true or false
+    string surveyName; // name of the survey
+    string question; // the question the user asks
     string[] solutions; // Written multiple choice options
     uint256[] answers; // collect answers
     uint256 numAllowedResponces; // how many people have answered
@@ -22,9 +24,47 @@ contract Survey {
         require(msg.sender == owner, 'call exclusive to owner');
     }
 
-    struct registered_user {
-        address userAddress;
-        string username;
-        uint password;
+    constructor(string memory _surveyName, 
+                string memory _question, 
+                string[] memory _solutions, 
+                uint256 _duration, 
+                uint256 _numAllowedResponces) {
+        owner = msg.sender;         //initialize all members of the survey
+        surveyName = _surveyName;
+        question = _question;
+        solutions = _solutions;
+        numAllowedResponces = _numAllowedResponces;
+        startTime = block.timestamp;
+        endTime = startTime + _duration;
+        ethReward = msg.value;
+
+        for (uint256 i = 0; i < _solutions.length; i++) { //initialize all answers to 0
+            answers.push(0);
+        }
+    
+    }
+
+
+     
+    function endNow() public onlyOwner {
+        endTime = block.timestamp;
+    }
+
+    function vote(uint256 option) public {
+        hasVoted[msg.sender] = true; //mark the user as having voted
+        answers[option] += 1; //increment the number of votes for the option
+        numAllowedResponces -= 1; //decrement the number of allowed responses remaining
+        if (numAllowedResponces == 0) { //if the number of allowed responses is 0, close the survey
+            endNow();
+        }
+    }
+
+    // Function to get the results of the survey
+    function getResults() public view returns (uint256[] memory) {
+        require(block.timestamp > endTime, "Survey is still active");
+
+        //print the question followed by its number of votes in answers
+
     }
 }
+
--- BlockSurveyors/src/SurveyContract.sol
+++ BlockSurveyors/src/SurveyContract.sol
@@ -9,9 +9,8 @@ import {Survey} from "../src/Survey.sol";
 contract SmartSurvey {
    
     //list of functions we must include:
-    //register
-    //create survey   (surveyName, surveyDescription, surveyOptions, surveyDuration, surveyReward);
-    //view survey (surveyName);
+    
+    //view survey (surveyName); should allow the user to view a survey and all its details before deciding to participate
 
     struct registered_user{
         address userAddress;
@@ -19,8 +18,24 @@ contract SmartSurvey {
         uint password;
     }
 
+    // Mapping to a survey (list?)
+    mapping(string => Survey) private user_surveys; //name of survey maps to 
+
+    // Event to be emitted when a new user registers
+    event UserRegistered(string name, address userAddress);
+
     // Mapping to store registered users for login
-    mapping(address => registered_user) private users;
+    mapping(address => registered_user) private users; //keep track of all registered users
+
+    // Event to log survey details (simulating print)
+    event SurveyDetails(
+        string surveyName,
+        string question,
+        string[] solutions,
+        uint256 startTime,
+        uint256 endTime,
+        uint256 numAllowedResponses
+    );
    
     function registerUser(string memory _username, uint256 _password) public {
         require(users[msg.sender].userAddress == address(0), "User already registered"); // make sure user is not already registered
@@ -36,33 +51,54 @@ contract SmartSurvey {
         emit UserRegistered(_username, msg.sender);
     }
 
-
-
-    // Mapping to a survey (list?)
-    mapping(address => uint256) private user_surveys;
-
-    // Event to be emitted when a new user registers
-    event UserRegistered(string name, address userAddress);
-
     
-    //should this function be payable?
-    function create_survey(string memory problems) public {
+    function create_survey(string memory _surveyName, 
+                          string memory _question, 
+                          string[] memory _solutions, 
+                          uint256 _duration, 
+                          uint256 _numAllowedResponces) public payable {
+        require(users[msg.sender].userAddress != address(0), "User not registered"); //make this function is exclusivly accessable to those who are registered already
         
-    }
-    
-    function vote(uint256 survey_id, uint256 option) public {
-        
-    }
-    
+        require(bytes(_surveyName).length > 0, "Survey name cannot be empty");
+        require(bytes(_question).length > 0, "Question cannot be empty");
+        require(_solutions.length > 0, "Solutions cannot be empty");
+        require(_duration > 0, "Duration must be greater than 0");
+        require(_numAllowedResponces > 0, "Number of allowed responses must be greater than 0");
+        require(msg.value > 0, "Reward must be greater than 0");
 
-
-    function get_result(uint256 survey_id) public {
-        
+        Survey survey = new Survey{value: msg.value}(_surveyName, _question, _solutions, _duration, _numAllowedResponces);
+        user_surveys[_surveyName] = survey;
     }
 
+    function viewSurvey(string memory _surveyName) public {
+        Survey survey = user_surveys[_surveyName];
+        require(address(survey) != address(0), "Survey does not exist");
 
-    constructor() {
-        owner = msg.sender;
-    }   
+        string memory surveyName = survey.surveyName();
+        string memory question = survey.question();
+        string[] memory solutions = survey.solutions();
+        uint256 startTime = survey.startTime();
+        uint256 endTime = survey.endTime();
+        uint256 numAllowedResponses = survey.numAllowedResponces();
+
+        emit SurveyDetails(surveyName, question, solutions, startTime, endTime, numAllowedResponses); //print the  information for the user 
+    }
+    
+    function vote(string memory _surveyName, uint256 option) public {
+        require(user_surveys[_surveyName].hasVoted[msg.sender] == false, "User has already voted");
+        require(user_surveys[_surveyName] != address(0), "Survey does not exist");
+        require(user_surveys[_surveyName].numAllowedResponces() > 0, "Survey is full");
+        require(option < user_surveys[_surveyName].solutions().length, "Invalid option");
+        require(user_surveys[_surveyName].endTime() > block.timestamp, "Survey is closed");
+        
+
+        user_surveys[_surveyName].vote(option);
+    }  
+
+
+    
+
+
+     
 
 }
